d02c28742553e79ae697eb88b7d911a1b021945b
Мельникова М.А. <mashkamelnikova0@gmail.com>
2026-03-15 17:01:02 +0300

    Добавлен класс Transaction

---
+++ BudgetManagement/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public enum TransactionType
+{
+    Доход,
+    Расход
+}
+
+public class Transaction
+{
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+    public TransactionType Type { get; set; }
+    public DateTime Date { get; set; }
+
+    public Transaction(string description, decimal amount, TransactionType type, DateTime date)
+    {
+        Description = description;
+        Amount = amount;
+        Type = type;
+        Date = date;
+    }
+}
+
+
+
